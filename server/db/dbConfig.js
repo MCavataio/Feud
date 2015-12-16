07df60c6dba88f44d0dcf064340d36e80431ec2b
mcavataio <Mcavataiosfp@gmail.com>
2015-12-16 11:18:31 -0800

    change to db

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -55,7 +55,7 @@ var Query = db.define('query', {
   response10: Sequelize.STRING,
 });
 
-db.sync({force:true})
+db.sync()
   .then(function(err) {
     console.log('created database')
   }, function (err) {
