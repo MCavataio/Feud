6d41ed4ea63eccf7a323603f2379693777aa67d6
mcavataio <Mcavataiosfp@gmail.com>
2015-12-15 16:38:37 -0800

    added new file

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require ('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
+var socket = require('./server/sockets/sockets.js')(io);
+
+
+require('./server/config/middleware.js')(app, express, io);
+
+
+var port = process.env.PORT || 3000;
+
+http.listen(port)
+
+console.log("connected to port: ",port)
+module.exports = app;
