8dd508536772db3ff8616ffbda5d3d976612e742
mcavataio <Mcavataiosfp@gmail.com>
2015-12-18 11:16:04 -0800

    testing pusher for socket

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var express = require ('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '161881',
+  key: '48afec95468f47903d06',
+  secret: '9244fc97634259f464f2',
+  encrypted: true
+});
+pusher.port = 443;
+
+pusher.trigger('test_channel', 'my_event', {
+  "message": "hello world"
+});
 
 var socket = require('./server/sockets/sockets.js')(io);
 
