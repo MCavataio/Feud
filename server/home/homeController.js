449b18ad697d84374abb7c6c42837b0da6ee47b4
mcavataio <Mcavataiosfp@gmail.com>
2015-12-27 14:28:20 -0500

    change to add user

--- server/home/homeController.js
+++ server/home/homeController.js
@@ -41,7 +41,7 @@ module.exports = {
   user: function(userInfo) {
     helpers.findOrCreateUser(userInfo)
     .then(function(user) {
-      Socket.emit(user);
+      Socket.emit('userInfo', user);
     })
   },
 
