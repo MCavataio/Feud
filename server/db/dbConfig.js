1d2cfc06be4a5f1401419cabf7bd1df7818b4a29
mcavataio <Mcavataiosfp@gmail.com>
2015-12-30 16:51:45 -0800

    updating database

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -87,7 +87,7 @@ var Query = db.define('query', {
   response10: Sequelize.STRING
 });
 
-db.sync({force: false})
+db.sync({force: true})
   .then(function(err) {
     console.log('created database')
   }, function (err) {
