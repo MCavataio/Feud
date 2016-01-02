be7085900756f5450073aa25abcbd4153f3188c3
mcavataio <Mcavataiosfp@gmail.com>
2016-01-02 11:42:50 -0800

    add logger

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require ('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http)
+var Promise = require('bluebird')
 
 
 var socket = require('./server/sockets/sockets.js')(io);
@@ -11,9 +12,10 @@ var socket = require('./server/sockets/sockets.js')(io);
 require('./server/config/middleware.js')(app, express, io);
 
 
-process.on('unhandledRejection', function(reason, p){
-    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
-    // application specific logging here
+Promise.onPossiblyUnhandledRejection(function(reason) {
+        // do some custom logging
+    console.log("possibly unhandled rejected Promise", reason);
+    process.exit(1);
 });
 
 var port = process.env.PORT || 3000;
