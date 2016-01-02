729ff89f717366e34cb06a08feec26b9db133547
mcavataio <Mcavataiosfp@gmail.com>
2016-01-02 11:17:13 -0800

    add catch

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ var socket = require('./server/sockets/sockets.js')(io);
 require('./server/config/middleware.js')(app, express, io);
 
 
+process.on('unhandledRejection', function(reason, p){
+    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    // application specific logging here
+});
+
 var port = process.env.PORT || 3000;
 
 http.listen(port)
